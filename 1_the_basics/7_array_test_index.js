e8bd4414a616d2755fe279bfc8dc9e644d907de3
Mike Anthony <mike@mikeanthony.com>
2021-05-25 19:20:30 -0500

    commit file 7

---
+++ 1_the_basics/7_array_test_index.js
@@ -0,0 +1,13 @@
+﻿/*
+Will an error occur if you try to access an array element with an index
+that is greater than or equal to the length of the array? For example:
+let foo = ['a', 'b', 'c'];
+console.log(foo.length);  // => 3
+console.log(foo[3]);      // will this result in an error?
+*/
+
+let foo = ['a', 'b', 'c'];
+console.log(foo.length);  // => 3
+console.log(foo[3]);      // will this result in an error?
+
+// Yep! index is zero based; so in this example the max index  is 2
