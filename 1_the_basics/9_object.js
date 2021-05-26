2b4a9b82724924b1dc62b06e640abd71063ea6b6
Mike Anthony <mike@mikeanthony.com>
2021-05-25 20:43:07 -0500

    commit file 9

---
+++ 1_the_basics/9_object.js
@@ -0,0 +1,6 @@
+﻿let pets = {cat: 'zargle', dog: 'drucker', lizard: 'sniffy', elephant: 'puffles'}
+// animal is the key, petname is the value
+
+
+console.log(pets['cat']);
+console.log(pets['dog']);
