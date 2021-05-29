fce378614d55717ee833fd13d018e5358081efb5
Mike Anthony <mike@mikeanthony.com>
2021-05-29 09:40:39 -0500

    commit new files

--- 1_the_basics/12_eval_greaterThan.js
+++ 1_the_basics/12_eval_greaterThan.js
@@ -1,3 +1,8 @@
 ﻿// '12' < '9' evaluates to what?
 
 console.log('12' < '9');
+
+//When you compare two strings, JavaScript performs a character-by-character 
+//comparison going from left to right, so on the first comparison, it determines that
+// 1 is less than 9
+//if we were comparing numbers it would be true
