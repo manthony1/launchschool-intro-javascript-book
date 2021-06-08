7d680d342efa62da29cf2a0e27da4e4366b0105a
Mike Anthony <mike@mikeanthony.com>
2021-06-08 10:54:07 -0500

    flow control

---
+++ 5_flow_control/ternary_operator1.js
@@ -0,0 +1,8 @@
+//let message = true ? 'this is true' : 'this is not true'
+//console.log(message);
+//console.log(false ? 'this is true' : 'this is not true')
+
+let x = 2;
+let y = 5;
+let check = (y > x) ? 'y is greater than x' : 'y is less than x'
+console.log(check);
