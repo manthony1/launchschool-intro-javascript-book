f2f71d04c73d47c556b8dc84ed3824257d52df49
Mike Anthony <mike@mikeanthony.com>
2021-06-08 21:06:01 -0500

    flow control

---
+++ 5_flow_control/3_evenorOdd-checktype.js
@@ -0,0 +1,13 @@
+function evenorOdd (input) {
+  if (!Number.isInteger(input)) {
+    return console.log('error');
+  }
+
+  if (input % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
+  }
+}
+
+evenorOdd(5);
