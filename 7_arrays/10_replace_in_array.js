a08ae3d9256584047c22b021bf45dae1aac9d12a
M. Anthony <mikeeanthony@gmail.com>
2021-07-02 14:41:21 -0500

    added function for replace in array

--- 7_arrays/10_replace_in_array.js
+++ 7_arrays/10_replace_in_array.js
@@ -6,6 +6,19 @@ let arr = [
   [4, 8, 12]
 ];
 
-console.log(arr);
-arr[1][3] = 606;
-console.log(arr);
+//console.log(arr);
+//arr[1][3] = 606;
+//console.log(arr);
+
+function findSix (arr) {
+  arr.forEach(element => {
+    for (let i = 0; i < element.length; i++) {
+      if (element[i] === 6) {
+        element[i] = 606;
+      }
+    } 
+  });
+  return console.log(arr);
+}
+
+findSix(arr);
